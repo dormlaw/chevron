a6c91c4ed6a30361b6bad575e0ba45ef8837c263
dormlaw <dormlaw@yandex.ru>
2023-03-27 00:02:31 +0300

    wordl done

--- bin/wordl.js
+++ bin/wordl.js
@@ -1,20 +1,49 @@
 const print = require('./colorizer')
 
 function wordl(words) {
+
+	const monitor = (type) => {
+		console.clear();
+		print('#yellow[> SLOVO] - Отгадай слово из 5 букв за 5 попыток!');
+		print('#c[введите #esc[exit] - чтобы закончить]');
+		print('');
+		print('К О #black[#bgred[Ф]] Т А - такой буквы в слове нет');
+		print('Т #black[#bgblue[А]] П О К - такая буква есть, но в другой позиции');
+		print('#black[#bggreen[П]] О Е З Д - такая буква есть и она в нужном месте');
+		print('');
+		print(`Осталось ${count} попыток`);
+		for (let i = 0; i < answers.length; i++)
+			print(answers[i].join(' '))
+		if (type === 'error-length') {
+			print('');
+			print('Слово должно состоять из 5 букв');
+			print('Попробуйте ещё:');
+		}
+		if (type === 'error-word') {
+			print('');
+			print('Такого слова нет в словаре...');
+			print('Попробуйте ещё:');
+		}
+		if (type === 'guess') {
+			print('');
+			print('Введите Вашу догадку:');
+		}
+		if (type === 'winner') {
+			print('#yellow[Поздравляю, это верное слово!]');
+		}
+		if (type === 'looser') {
+			print('К сожалению все попытки закончились');
+			print(`Верное слово было: ${riddle.join(' ')}`);
+		}
+	}
+
 	//starting code
-	console.clear();
+	let riddleWord = words[Math.floor(Math.random() * words.length)];
+	let riddle = riddleWord.split('');
 	let count = 5;
-	print('#yellow[> SLOVO] - Отгадай слово из 5 букв за 5 попыток!');
-	print('#c[введите #esc[exit] - чтобы закончить]');
-	print('');
-	print('К О #black[#bgred[Ф]] Т А - такой буквы в слове нет');
-	print('Т #black[#bgblue[А]] П О К - такая буква есть, но в другой позиции');
-	print('#black[#bggreen[П]] О Е З Д - такая буква есть и она в нужном месте');
-	print('');
-	print(`Осталось ${count} попыток`);
-	print('');
-	print('Введите Вашу догадку:');
-	let riddle = words[Math.floor(Math.random() * words.length)];
+	let answers = []
+	monitor('guess');
+
 	//gameflow
 	process.stdin.on('data', (data) => {
 		const word = data.toString().toLowerCase().trim();
@@ -22,14 +51,36 @@ function wordl(words) {
 			process.stdin.removeAllListeners('data');
 			process.exit();
 		}
-		if (word === riddle) {
-			print('#yellow[Поздравляю, это верное слово!]');
-			process.stdin.removeAllListeners('data');
-			// TO DO
+		if (word.length === 5) {
+			if (words.includes(word)) {
+				let answer = word.split('');
+				for (let i = 0; i < 5; i++) {
+					if (answer[i] === riddle[i]) {
+						answer[i] = '#black[#bggreen[' + answer[i].toUpperCase() + ']]'
+					} else if (riddle.includes(answer[i])) {
+						answer[i] = '#black[#bgblue[' + answer[i].toUpperCase() + ']]'
+					} else {
+						answer[i] = '#black[#bgred[' + answer[i].toUpperCase() + ']]'
+					}
+				};
+				answers.push(answer);
+				count--;
+				if (word === riddleWord) {
+					count--
+					monitor('winner')
+					process.stdin.removeAllListeners('data');
+					process.exit();
+				}
+				monitor('guess');
+			} else {
+				monitor('error-word')
+			}
+		} else {
+			monitor('error-length');
 		}
-		
-
-
+		if (count === 0) {
+			monitor('looser')
+		}
 	});
 }
 

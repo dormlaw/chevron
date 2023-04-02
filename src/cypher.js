3942a14c30be6c4154a951645bb9c372a7a6f5a3
dormlaw <dormlaw@yandex.ru>
2023-04-02 22:04:57 +0300

    working enigma code, without rotor spins

--- src/cypher.js
+++ src/cypher.js
@@ -1,59 +1,83 @@
 const enigma = {
-  alphabet: 'йцукенгшщзхъфывапролджэячсмитьбю.,!?_ё',
-  reflector: 'ё?зждлор.павфцим!счяуъкнэщ,бгшфйтю_еых',
+  alphabet: 'abcdefghijklmnopqrstuvwxyz',
+  reflector:
+    [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19],
   rotors: [
-    {
-      letters: '.эькмъчжаюхйшртнфыоуэвсздгпибёцщея!,?_',
-      offset: 1,
-    },
-    {
-      letters: 'чуре.ъщ!,жбхлсаозныфдвтпияшмгкю?цйэьё_',
-      offset: 3,
-    },
-    {
-      letters: '_йэгзврул!яфщохтпъмде,ычнасикшьжбцё?ш.',
-      offset: 2,
-    }
-  ]
+    [1, 17, 10, 2, 5, 3, 23, 12, 13, 20, 6, 9, 22, 8, 19, 15, 16, 14, 11, 4, 25, 21, 0, 7, 18, 24],
+    [17, 8, 5, 12, 7, 21, 23, 20, 24, 0, 2, 22, 1, 10, 9, 15, 11, 3, 25, 4, 6, 14, 16, 13, 19, 18],
+    [24, 3, 22, 14, 7, 10, 21, 0, 9, 2, 8, 15, 18, 11, 19, 1, 23, 6, 20, 16, 17, 25, 12, 4, 13, 5],
+  ],
 };
 
-function encrypt(data, enigma, key = 'aaa') {
-  const dataString = data.toString();
-  const preparedArray = dataString.toLowerCase()
+function encrypt(message, { alphabet, reflector, rotors }, key = 'abc') {
+  // preparing data for encryption
+  // all letters to lower case
+  // swap <space> with '_'
+  // split to array for further work
+  const messageString = message.toString();
+  const messageArray = messageString.toLowerCase()
     .split('')
-    .map((letter) => {
-      let preparedLetter;
-      if (letter === ' ') {
-        preparedLetter = '_';
-      } else preparedLetter = letter;
-      return preparedLetter;
-    });
+    .map((letter) => (letter === ' ' ? '_' : letter));
+
+  console.log(messageArray);
+
+  // get starting points of rotors depending of key
   const rotationCounter = [];
-  enigma.rotors.forEach((rotor) => {
-    console.log(enigma.rotors[enigma.rotors.indexOf(rotor)].letters.charAt('a'))
-    console.log(enigma.rotors[rotor].letters.indexOf('а'))
-    rotationCounter.push(
-      enigma.rotors[rotor].indexOf(key[enigma.rotors.indexOf(rotor)])
-    )
-  });
-  let encryptedArray;
-  for (letter of preparedArray) {
-    let encryptedLetter;
-    if (enigma.alphabet.includes(letter)) {
-      const reflectorIndex = enigma.alphabet.indexOf(letter);
-      encryptedLetter = enigma.reflector[reflectorIndex];
-      enigma.rotors.forEach((rotor) => {
-        const rotorOffset = 1
-        const rotorIndex = enigma.rotors[rotor].indexOf(encryptedLetter + rotorOffset);
-        encryptedLetter = enigma.rotors[rotor][rotorIndex];
-        rotationCounter[enigma.rotors.indexOf(rotor)] += 1;
-      });
+  rotors.forEach((rotor) => {
+    const rotorKey = key[rotors.indexOf(rotor)];
+    const rotorStartingPoint = alphabet.indexOf(rotorKey);
+    if (rotorStartingPoint !== -1) {
+      rotationCounter.push(rotorStartingPoint);
     } else {
-      encryptedArray.push(letter);
+      rotationCounter.push(0);
     }
+  });
+
+  console.log(rotationCounter);
+
+  // start encrypting
+  const encryptedArray = [];
+  for (let i = 0; i < messageArray.length; i += 1) {
+    const letter = messageArray[i];
+    const inputIndex = alphabet.indexOf(letter);
+    console.log(inputIndex);
+    let encryptedLetter = letter;
+    if (inputIndex !== -1) {
+      encryptedLetter = rotors[0][inputIndex];
+      console.log(encryptedLetter);
+      encryptedLetter = rotors[1][encryptedLetter];
+      console.log(encryptedLetter);
+      encryptedLetter = rotors[2][encryptedLetter];
+      console.log(encryptedLetter);
+      encryptedLetter = reflector[encryptedLetter];
+      console.log(encryptedLetter);
+      encryptedLetter = rotors[2].indexOf(encryptedLetter);
+      console.log(encryptedLetter);
+      encryptedLetter = rotors[1].indexOf(encryptedLetter);
+      console.log(encryptedLetter);
+      encryptedLetter = rotors[0].indexOf(encryptedLetter);
+      console.log(encryptedLetter);
+      encryptedLetter = alphabet[encryptedLetter];
+    }
+    encryptedArray.push(encryptedLetter);
+    // console.log(letter);
+    // console.log(inputIndex);
+    console.log(encryptedLetter);
   }
-  const encryptedString = encryptedArray.join('');
-  return encryptedString
+
+  // return <spaces> and capital letters
+  // stringify encrypted message
+  const encryptedMessage = encryptedArray.map((letter, index) => {
+    if (letter === '_') {
+      return ' ';
+    }
+    if (messageString[index] === messageString[index].toUpperCase()) {
+      return letter.toUpperCase();
+    }
+    return letter;
+  }).join('');
+
+  return encryptedMessage;
 }
 
-console.log(encrypt('привет, братишка!', enigma));
+console.log(encrypt('Hello, bro!', enigma));
